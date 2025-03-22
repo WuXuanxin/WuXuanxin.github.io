443d48bca365e66a2e5eea68030a1abbabb350ab
WuXuanxin <111167982+WuXuanxin@users.noreply.github.com>
2025-03-22 13:45:43 +0000

    Deploying to gh-pages from @ WuXuanxin/WuXuanxin.github.io@b8aad5df4a64eac23ccdf3fe1a7adddba3a76d4c 🚀

--- jp/assets/js/search-data.js
+++ jp/assets/js/search-data.js
@@ -36,7 +36,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "ナビゲーションメニュー",
           handler: () => {
             window.location.href = "/jp/cv/";
